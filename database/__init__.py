2bbbb61c68dae920f3b3c340a85c8b41cfa50a1b
Mathias <mathias@pop-os.localdomain>
2020-04-22 16:15:15 +0200

    updated redis

--- database/__init__.py
+++ database/__init__.py
@@ -5,7 +5,8 @@ from flask import g
 def connect_redis():
     try:
         if 'redis' not in g:
-            g.redis = StrictRedis(host='redis', port=6379, decode_responses=True)
+            g.redis = StrictRedis(host='ffc_backend_redis',
+                                  port=6379, decode_responses=True)
         return g.redis
     except Exception as ex:
         print('Failed to connect redis: {}'.format(ex))
