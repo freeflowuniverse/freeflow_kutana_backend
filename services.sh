53894d86fa9c9acdfb50effd6a16a00b119ce626
Mathias <mathias@pop-os.localdomain>
2020-04-22 16:10:43 +0200

    callable re-added

--- services.sh
+++ services.sh
@@ -1,2 +1,2 @@
-cd /usr/share/nginx/ && uwsgi --disable-logging --http :5000 --gevent 1000 --http-websockets -M --wsgi-file app.py &
+cd /usr/share/nginx/ && uwsgi --disable-logging --http :5000 --gevent 1000 --http-websockets -M --wsgi-file app.py --callable app &
 nginx -g 'daemon off;'
