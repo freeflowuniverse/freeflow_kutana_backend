7d39e21d86c8342f13efbb4fa8122fc0c50c4afc
Mathias <mathias@pop-os.localdomain>
2020-04-23 12:30:11 +0200

    docker-compose configuration ci/cd

--- services.sh
+++ services.sh
@@ -1,2 +1,2 @@
-cd /usr/share/nginx/ && uwsgi --disable-logging --http :5000 --gevent 1000 --http-websockets -M --wsgi-file app.py --callable app &
+cd /usr/share/nginx/ && uwsgi --disable-logging --http :5000 --gevent 1000 --http-websockets --master --wsgi-file app.py --callable app &
 nginx -g 'daemon off;'
