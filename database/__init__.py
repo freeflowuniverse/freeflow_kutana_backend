9bb5c6b30be83104d8c35224adbd1544b206171d
tobiaschielens <tobias.chielens@gmail.org>
2020-10-06 18:03:44 +0200

    updated redis host

--- database/__init__.py
+++ database/__init__.py
@@ -4,7 +4,7 @@ from flask import g
 
 def connect_redis():
     try:
-        if 'redis' not in g:g.redis = StrictRedis(host='172.20.0.10', port=6379, decode_responses=True)
+        if 'redis' not in g:g.redis = StrictRedis(host='127.0.0.1', port=6379, decode_responses=True)
         return g.redis
     except Exception as ex:
         print('Failed to connect redis: {}'.format(ex))
